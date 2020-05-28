afe473c0ccf01c016871c6bcc8f8736ffb75feff
Matteo Fumagalli <matteofumagalli1275@gmail.com>
2020-05-28 20:56:24 +0200

    Fix pypi not reading readme

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 import setuptools
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(name='jankigen',
-                 version='0.1.0',
+                 version='0.1.1',
                  packages=setuptools.find_packages(),
                  entry_points={
                      'console_scripts': [
@@ -13,4 +19,7 @@ setuptools.setup(name='jankigen',
                      'jamdict',
                      'janome'
                  ],
+                 description="Generate Anki decks from Japanese text",
+                 long_description=long_description,
+                 long_description_content_type='text/markdown'
                  )
